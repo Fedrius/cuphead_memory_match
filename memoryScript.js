ead14bb4ef9b072a70388e16f411b47a91fed494
Paul <paulmlee7@gmail.com>
2017-12-14 22:30:30 -0800

    Memory match version1.0 - Paul Lee

--- memoryScript.js
+++ memoryScript.js
@@ -7,38 +7,69 @@ var firstCardClicked = 0,
     storedCard = null,
     matches = 0,
     attempts = 0,
-    accuracy = 0;
+    accuracy = 0 + '%',
     gamesPlayed = 9000;
 
+var randomCardArray = ['Blind_Specter.png',
+    'Cagney_carnation_2.png',
+    'Cala_maria.png',
+    'Croak.png',
+    'Devil.png',
+    'Honeybottoms.jpg',
+    'KingTheDice.jpg',
+    'Match.png',
+    'Psycarrot_brain_minding.png',
+    'Blind_Specter.png',
+    'Cagney_carnation_2.png',
+    'Cala_maria.png',
+    'Croak.png',
+    'Devil.png',
+    'Honeybottoms.jpg',
+    'KingTheDice.jpg',
+    'Match.png',
+    'Psycarrot_brain_minding.png'];
+
+var storedCardArray = randomCardArray.slice();
+
 function initializeApp() {
-    $('.card').click(cardClicked);
+    createCards();
+    applyCardClickHandler();
     displayStats();
     $('.reset').click(resetButton);
+}
+
+function applyCardClickHandler() {
+    $('.card').click(cardClicked);
 }
 
 function displayStats() {
     $('.games-played .value').text(gamesPlayed);
     $('.attempts .value').text(attempts);
 
-    if(accuracy === 0){
-        accuracy = 0 + '%';
-    }else {
-        accuracy=  accuracy.toFixed(2) + '%';
+    if(matches > 0 || attempts > 0){
+        accuracy = ((matches / attempts) * 100).toFixed(2) + '%';
     }
     $('.accuracy .value').text(accuracy);
 }
 
 function resetStats() {
+    storedCard = null;
+    firstCardClicked = 0;
+    secondCardClicked = 0;
+    matchCounter = 0;
     matches = 0;
     attempts = 0;
+    accuracy = 0 + '%';
     displayStats();
 }
 
 function resetButton() {
     gamesPlayed++;
-    resetStats()
+    resetStats();
+    $('.card').remove();
+    createCards();
+    applyCardClickHandler();
 }
-
 
 function cardClicked() {
 
@@ -83,21 +114,33 @@ function cardClicked() {
 
                 $(storedCard).find('.back').show();
                 $(cardHideBack).find('.back').show();
-
-                $('.card').on('click', cardClicked);
-
+                applyCardClickHandler();
                 storedCard = null;
 
             }, 2000);
             firstCardClicked = 0;
             secondCardClicked = 0;
             console.log('work??')
-
         }
     }
 }
 
+function createCards() {
+    while(randomCardArray.length > 0){
 
+        var randomizedCardIndex = Math.floor(Math.random() * (randomCardArray.length));
+        var newDiv = $('<div>').addClass('card');
+        var newFront = $('<img>').addClass('front').attr('src',randomCardArray[randomizedCardIndex]);
+        var newBack = $('<img>').addClass('back').attr('src', "Cupheadcardback.png");
+        randomCardArray.splice(randomizedCardIndex, 1);
+
+        newDiv.append(newFront);
+        newDiv.append(newBack);
+        $('#game-area').append(newDiv);
+    }
+
+randomCardArray = storedCardArray.slice();
+}
 
 
 
