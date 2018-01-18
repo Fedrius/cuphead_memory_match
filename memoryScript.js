d3d4f2a86a0c1ec8cef92f299285f78b57cbae2b
Paul <paulmlee7@gmail.com>
2018-01-18 10:52:09 -0800

    updated game to es6

--- memoryScript.js
+++ memoryScript.js
@@ -1,51 +1,53 @@
 $(document).ready(initializeApp);
+
 let memoryGame = null;
+
 function initializeApp(){
     memoryGame = new MemoryGame();
     memoryGame.init();
 }
 
+class MemoryGame{
+    constructor(){
+        this.firstCardClicked = 0;
+        this.secondCardClicked = 0;
+        this.matchCounter = 0;
+        this.totalPossibleMatches = 9;
+        this.storedCard = null;
+        this.matches = 0;
+        this.attempts = 0;
+        this.accuracy = 0 + '.00%';
+        this.gamesPlayed = 9000;
+        this.setTimeOutTimer = null;
+        this.cupheadOrMugmanCard = ["Cupheadcardback.png", "mugmanCard.jpg"];
+        this.randomCardArray = ['Blind_Specter.png',
+            'Cagney_carnation_2.png',
+            'Cala_maria.png',
+            'Croak.png',
+            'Devil.png',
+            'Honeybottoms.jpg',
+            'KingTheDice.jpg',
+            'Match.png',
+            'Psycarrot_brain_minding.png',
+            'Blind_Specter.png',
+            'Cagney_carnation_2.png',
+            'Cala_maria.png',
+            'Croak.png',
+            'Devil.png',
+            'Honeybottoms.jpg',
+            'KingTheDice.jpg',
+            'Match.png',
+            'Psycarrot_brain_minding.png'];
+    }
 
-
-function MemoryGame(){
-    this.firstCardClicked = 0;
-    this.secondCardClicked = 0;
-    this.matchCounter = 0;
-    this.totalPossibleMatches = 9;
-    this.storedCard = null;
-    this.matches = 0;
-    this.attempts = 0;
-    this.accuracy = 0 + '.00%';
-    this.gamesPlayed = 9000;
-    this.setTimeOutTimer = null;
-    this.cupheadOrMugmanCard = ["Cupheadcardback.png", "mugmanCard.jpg"];
-    this.randomCardArray = ['Blind_Specter.png',
-                            'Cagney_carnation_2.png',
-                            'Cala_maria.png',
-                            'Croak.png',
-                            'Devil.png',
-                            'Honeybottoms.jpg',
-                            'KingTheDice.jpg',
-                            'Match.png',
-                            'Psycarrot_brain_minding.png',
-                            'Blind_Specter.png',
-                            'Cagney_carnation_2.png',
-                            'Cala_maria.png',
-                            'Croak.png',
-                            'Devil.png',
-                            'Honeybottoms.jpg',
-                            'KingTheDice.jpg',
-                            'Match.png',
-                            'Psycarrot_brain_minding.png'];
-
-    this.init = function(){
+    init(){
         this.createCards();
         this.applyCardClickHandler();
         this.buttonAndModalClickHandlers();
         this.displayStats();
-    };
+    }
 
-    this.buttonAndModalClickHandlers = function(){
+    buttonAndModalClickHandlers(){
         $('.reset').on('click', ()=>this.resetButton());
         $('.winnerModal').on('click', ()=>this.hideWinner());
         $(window).keydown(function(){
@@ -53,37 +55,37 @@ function MemoryGame(){
         });
         $('.aboutMe>span').on('click', ()=>this.showAboutMe());
         $('.close').on('click', ()=>this.hideAboutMe());
-    };
+    }
 
-    this.showAboutMe = function(){
+    showAboutMe(){
         $('.aboutMeModal').fadeIn(200);
-    };
+    }
 
-    this.hideAboutMe = function(){
+    hideAboutMe(){
         $('.aboutMeModal').fadeOut(200);
-    };
+    }
 
-    this.showWinner = function(){
+    showWinner(){
         $('.winnerModal').fadeIn(1000);
-    };
+    }
 
-    this.hideWinner = function(){
+    hideWinner(){
         $('.winnerModal').fadeOut(500);
-    };
+    }
 
-    this.applyCardClickHandler = function(){
-        $('.card').on('click', this.cardClicked.bind(this));
-    };
+    applyCardClickHandler(){
+        $('.card').on('click', ()=>this.cardClicked(event));
+    }
 
-    this.toggleClickOnOff = function(card){
+    toggleClickOnOff(card){
         $(card).toggleClass('disableClick');
-    };
+    }
 
-    this.clearTimeOut = function(){
+    clearTimeOut(){
         clearTimeout(this.setTimeOutTimer);
-    };
+    }
 
-    this.displayStats = function(){
+    displayStats(){
         $('.games-played .value').text(this.gamesPlayed);
         $('.attempts .value').text(this.attempts);
 
@@ -91,9 +93,9 @@ function MemoryGame(){
             this.accuracy = ((this.matches / this.attempts) * 100).toFixed(2) + '%';
         }
         $('.accuracy .value').text(this.accuracy);
-    };
+    }
 
-    this.resetStats = function(){
+    resetStats(){
         this.storedCard = null;
         this.firstCardClicked = 0;
         this.secondCardClicked = 0;
@@ -102,9 +104,9 @@ function MemoryGame(){
         this.attempts = 0;
         this.accuracy = 0 + '.00%';
         this.displayStats();
-    };
+    }
 
-     this.resetButton = function(){
+    resetButton(){
         this.clearTimeOut();
         this.gamesPlayed++;
         this.resetStats();
@@ -112,9 +114,9 @@ function MemoryGame(){
         $('.card').remove();
         this.createCards();
         this.applyCardClickHandler();
-    };
+    }
 
-     this.createCards = function(){
+    createCards(){
         let storedCardArray = this.randomCardArray.slice();
         let cupmanOrMugmanIndex = Math.floor(Math.random() * 2);
 
@@ -134,22 +136,21 @@ function MemoryGame(){
         }
 
         this.randomCardArray = storedCardArray.slice();
-    };
+    }
 
-    this.cardClicked = function(event) {
-
+    cardClicked(event){
         let matchCard = event.currentTarget;
         $(event.currentTarget).toggleClass('transformBack hover');
         let frontCard = event.currentTarget.firstChild;
 
-        if (this.firstCardClicked === 0) {
+        if (this.firstCardClicked === 0){
             this.firstCardClicked = $(frontCard).attr('src');
             this.toggleClickOnOff(event.currentTarget);
             this.storedCard = matchCard;
             return;
         }
 
-        if (this.firstCardClicked !== 0) {
+        if (this.firstCardClicked !== 0){
             this.attempts++;
             this.displayStats();
             this.secondCardClicked = $(frontCard).attr('src');
@@ -172,9 +173,9 @@ function MemoryGame(){
                 this.secondCardClicked = 0;
             }
         }
-    };
+    }
 
-    this.setTimer = function(temp){
+    setTimer(temp){
         let theObject = this;
         this.setTimeOutTimer = setTimeout(function(){
             $(theObject.storedCard).toggleClass('transformBack hover');
@@ -184,5 +185,5 @@ function MemoryGame(){
             theObject.toggleClickOnOff(theObject.storedCard);
             theObject.storedCard = null;
         }, 1000);
-    };
+    }
 }
